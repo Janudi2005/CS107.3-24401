ef365adfb2fff8b044978543d4402ecfa1c319f9
Janudi2005 <104355114+Janudi2005@users.noreply.github.com>
2023-06-06 17:48:54 +0530

    All Labs

---
+++ Lab 1 - 02.05.2023/Question 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            /*Console.WriteLine("Hello world!");*/ //printing hello world
+            /* int myAge;//declaring a variable called int
+             myAge = 18;
+             Console.WriteLine("Your age is: " +myAge);*/
+            //Q1
+            string name = "janudi";
+            Console.WriteLine("Your name is: "+name);
+            //Q2
+            int myAge = 18;
+            Console.WriteLine("Your name is:"+name +"\nYour age is:"+myAge);
+            //Q3  
+            Console.WriteLine("*\n**\n***\n****\n*****");
+            
+            //Q4
+            Console.Write("\n*");
+            Console.Write("\n**");
+            Console.Write("\n***");
+            Console.Write("\n****");
+            Console.Write("\n*****");
+
+            //Q5
+            Console.WriteLine();
+            string myName = "Janudi";
+            int age = 18;
+            string myCity = "Colombo";
+            Console.WriteLine("Your name is:" +myName +"\tYour age is:"+ age +"\tYour city is:"+myCity);
+            Console.ReadKey();
+
+        }
+    }
+}
