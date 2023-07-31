179fcc9d2d731152004f5a96ec60a368f9d2b7d9
Janudi2005 <104355114+Janudi2005@users.noreply.github.com>
2023-07-31 19:45:42 +0530

    C# lab 9

---
+++ Lab 9 - 11.07.2023/Question 1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee();
+ 
+            Console.Write("Enter Employee ID: ");
+            int empID = Convert.ToInt32(Console.ReadLine());
+            employee.SetEmpID(empID);
+
+            Console.Write("Enter Employee Name: ");
+            string empName = Console.ReadLine();
+            employee.SetEmpName(empName);
+
+            Console.Write("Enter Employee Designation: ");
+            string empDesignation = Console.ReadLine();
+            employee.SetEmpDesignation(empDesignation);
+
+            
+            Console.WriteLine("Hello " + employee.GetEmpName() + "! Your Employee ID is " + employee.GetEmpID() + " and you are a " + employee.GetEmpDesignation() + ".");
+ 
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        private int empID;
+        private string empName;
+        private string empDesignation;
+
+        public int GetEmpID()
+        {
+            return empID;
+        }
+
+        public void SetEmpID(int empID)
+        {
+            this.empID = empID;
+        }
+
+        public string GetEmpName()
+        {
+            return empName;
+        }
+
+        public void SetEmpName(string empName)
+        {
+            this.empName = empName;
+        }
+
+        public string GetEmpDesignation()
+        {
+            return empDesignation;
+        }
+
+        public void SetEmpDesignation(string empDesignation)
+        {
+            this.empDesignation = empDesignation;
+        }
+    }       
+  }
+ 
+    
+
+ 
---
+++ Lab 9 - 11.07.2023/Question 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class Calculator
+        {
+            private int num1;
+            private int num2;
+
+            public int   Operand1
+            {
+                get { return num1; }
+                set { num1 = value; }
+            }
+
+            public int Operand2
+            {
+                get { return  num2; }
+                set { num2 = value; }
+            }
+
+            public int Addition()
+            {
+                return num1 + num2;
+            }
+
+            public int Subtraction()
+            {
+                return num1 - num2;
+            }
+
+            public int Multiplication()
+            {
+                return  num1 * num2;
+            }
+
+            public int Division()
+            {
+                if (num2 == 0)
+                {
+                    throw new DivideByZeroException("Cannot divide by zero.");
+                }
+
+                return  num1 /  num2;
+            }
+        }
+    }
+}
