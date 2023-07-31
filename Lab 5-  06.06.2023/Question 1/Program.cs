18117c687efbe5c4315a2348f87912aabce15568
Janudi2005 <104355114+Janudi2005@users.noreply.github.com>
2023-07-31 19:27:19 +0530

    C# lab 5

---
+++ Lab 5-  06.06.2023/Question 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student objStudent = new student();
+            objStudent.myMethod();
+            objStudent.myInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 5-  06.06.2023/Question 1/student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class student
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Hi!");
+        }
+        public void myInfo() 
+        {
+            Console.WriteLine("Name: Janudi");
+            Console.WriteLine("Batch: 22.2 - FOC");
+            Console.WriteLine("index no: 24401");
+
+        }
+        
+    }
+}
