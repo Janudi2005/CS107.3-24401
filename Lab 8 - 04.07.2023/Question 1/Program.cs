185fc29aa3eb9c00ebe643a3fa9250545b15414f
Janudi2005 <104355114+Janudi2005@users.noreply.github.com>
2023-07-31 19:44:50 +0530

    C# lab 8

---
+++ Lab 8 - 04.07.2023/Question 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            arrayClass obj1 = new arrayClass();
+            int[] array = obj1.arrayMethod(size);
+
+            Console.WriteLine("\nDisplay the array: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + "");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 8 - 04.07.2023/Question 1/arrayClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    internal class arrayClass
+    {
+        public int[] arrayMethod(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                Console.Write($"Enter a value{i+1} :");
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                array[i] = value;
+                array[i + 1] = 0;
+            }
+
+            return array;
+        }
+            
+           
+          
+           
+           
+    }
+}
